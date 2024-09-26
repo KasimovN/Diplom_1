bcd0e47966b37d2d40761ca202fc7a8f612cb577
Nikolai Kasimov <kasimov.nn@yandex.ru>
2024-09-27 00:43:23 +0400

    Прописаны unit-test для database.py

--- data.py
+++ data.py
@@ -8,3 +8,7 @@ class StarBurgersData:
     RANDOM_NAME = Helper.random_name(NAME_LEGTH)
     RANDOM_PRICE = Helper.random_price(MAX_PRICE)
     RANDOM_INGREDIENT_TYPE = Helper.random_ingredient_type(INGREDIENT_TYPES)
+    DATABASE_BUN_NAME_LIST = ["black bun", "white bun", "red bun"]
+    DATABASE_BUN_SUM_PRICE = 100*1 + 200*2 + 300*3
+    DATABASE_INGREDIENT_NAME_LIST = ["hot sauce", "sour cream", "chili sauce", "cutlet", "dinosaur", "sausage"]
+    DATABASE_INGREDIENT_SUM_PRICE = 100*1 + 200*2 + 300*3 + 100*4 + 200*5 + 300*6
